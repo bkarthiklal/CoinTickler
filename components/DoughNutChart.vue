9abcb2d2fb8e1b715b1f3733fe2bc98deeb6f72c
Karthik <bkarthiklal@gmail.com>
2022-03-08 19:55:02 +0530

    Created a component for doughnut chart

---
+++ components/DoughNutChart.vue
@@ -0,0 +1,31 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+      default: null
+    },
+    options: {
+      type: Object,
+      required: false,
+      default() {
+        return {
+          responsive: true,
+          maintainAspectRatio: false,
+          animation: {
+            animateRotate: true,
+          }
+        }
+      }
+
+    },
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
