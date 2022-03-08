f43e857857a50362cd13839968f90e02dec8d7b7
Karthik <bkarthiklal@gmail.com>
2022-03-08 19:56:02 +0530

    Created a component to render Results

---
+++ components/ResultBlock.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- Results Block  -->
+  <div class="result-block">
+    <div class="result-row">
+      <div class="result-label">Invested amount</div>
+      <div class="result-value">
+       <span>{{ investedAmount | currency }}</span>
+      </div>
+    </div>
+    <div class="result-row">
+      <div class="result-label">Est. returns</div>
+      <div class="result-value">
+       <span>{{ expectedReturns | currency }}</span>
+      </div>
+    </div>
+    <div class="result-row">
+      <div class="result-label">
+        <strong>
+          Total value
+        </strong>
+      </div>
+      <div class="result-value">
+       <span>{{ totalValue | currency }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultBlock',
+  filters: {
+    currency(value) {
+      if (typeof value !== "number") {
+          return value;
+      }
+      const formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'INR'
+      });
+      return formatter.format(value).slice(0, -3);
+    }
+  },
+  props: {
+    investedAmount: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    expectedReturns: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    totalValue: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+  },
+
+}
+</script>
