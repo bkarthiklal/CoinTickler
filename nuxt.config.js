3d39c5e95bab8538e45eb6dba5959cab6cd89a4c
Karthik <bkarthiklal@gmail.com>
2022-03-08 19:54:23 +0530

    Installed and registered Vue-chart and Range-Slider

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   css: ['~/scss/style.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/RangeSlider.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
