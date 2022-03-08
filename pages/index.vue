f23b0248005afd97e4e4207f62dd1fbfe14d23e1
Karthik <bkarthiklal@gmail.com>
2022-03-08 19:56:56 +0530

    Created a Tab Ui for different Logic to render

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <Tutorial />
+  <div class="root_template">
+    <div class="app-container">
+      <h1 class="app-title">SIP Calculator</h1>
+      <b-tabs content-class="mt-3" lazy fill>
+        <b-tab title="SIP">
+          <keep-alive>
+            <TabSip />
+          </keep-alive>
+        </b-tab>
+        <b-tab title="Lumpsum">
+          <keep-alive>
+            <TabLumpSum />
+          </keep-alive>
+        </b-tab>
+        <b-tab title="Step Up Sip">
+          <keep-alive>
+            <TabStepUpSip />
+          </keep-alive>
+        </b-tab>
+        <b-tab title="Inflation">
+          <b-alert show>Inflation</b-alert>
+        </b-tab>
+        <b-tab title="Deflation">
+          <b-alert show>Deflation</b-alert>
+        </b-tab>
+        <b-tab title="CARG">
+          <b-alert show>CARG</b-alert>
+        </b-tab>
+      </b-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
