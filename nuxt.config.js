d700da49e73b745fe498a205a3437a2179b51d0d
Karthik <bkarthiklal@gmail.com>
2022-03-28 01:00:07 +0530

    Changing app name

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Investment Calculator',
+    title: 'Coin Tickler',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
